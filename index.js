f33dc71855b3d7af481ca85e44df49b7dde4d695
Shrawan <69075276+shrawan-beginner@users.noreply.github.com>
2025-09-06 11:07:54 +0545

    Refactor: Player Selection Event Listener
    - Refactor the player option selector into one function and remove
      previous eventListener calls separately for each of the options
    - Remove the variables for the option DOM elements(like rockSelected)

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+// DOM Elements Called to their own variables
 const startButton = document.querySelector('.btn-start');
 const gameWindow = document.querySelector('.game-window');
 const resetButton = document.querySelector('.btn-reset');
-const rockSelected = document.querySelector('.selection-rock');
-const paperSelected = document.querySelector('.selection-paper');
-const scissorSelected = document.querySelector('.selection-scissor');
+const selectionButtons = document.querySelector('.selection-buttons').querySelectorAll('button');
 const resultText = document.querySelector('.result-text');
 const player = document.querySelector('#player');
 const computer = document.querySelector('#computer');
@@ -11,7 +10,6 @@ let playerScore = player.querySelector('.score');
 let computerScore = computer.querySelector('.score');
 let playerSelection;
 let computerSelection;
-
 
 startButton.addEventListener('click', function (e) {
     if (gameWindow === undefined) return;
@@ -27,20 +25,21 @@ resetButton.addEventListener('click', function (e) {
     computerScore.innerText = '0';
 });
 
-rockSelected.addEventListener('click', function (e) {
-    playerSelection = "Rock";
-    compareChoices(playerSelection, computerChoice())
+// Player Selection for Rock, Paper, Scissors Buttons
+selectionButtons.forEach(function (button) {
+    button.addEventListener('click', handleClick);
 });
 
-paperSelected.addEventListener('click', function (e) {
-    playerSelection = "Paper";
-    compareChoices(playerSelection, computerChoice())
-});
-
-scissorSelected.addEventListener('click', function (e) {
-    playerSelection = "Scissors";
-    compareChoices(playerSelection, computerChoice())
-});
+function handleClick(e) {
+    if (e.target.matches('.selection-rock')) {
+        playerSelection = "Rock";
+    } else if (e.target.matches('.selection-paper')) {
+        playerSelection = "Paper";
+    } else if (e.target.matches('.selection-scissor')) {
+        playerSelection = "Scissors"
+    }
+    compareChoices(playerSelection, computerChoice());
+}
 
 function computerChoice() {
     let options = ['Rock', "Paper", "Scissors"];
